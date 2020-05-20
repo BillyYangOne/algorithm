f43349f2a9ed5bdf9313c88a19fa712774ffb44b
Billy <you199037@163.com>
2020-05-20 11:10:40 +0800

    滑动窗口算法

--- src/com/billy/leetcode/SlidingWindow.java
+++ src/com/billy/leetcode/SlidingWindow.java
@@ -12,8 +12,7 @@ public class SlidingWindow {
 
     public static void main(String[] args) {
 
-        SlidingWindow slidingWindow = new SlidingWindow();
-        String s = slidingWindow.minWindowSubStr("ADOBECODEBANC", "ABC");
+        String s = minWindowSubStr("ADOBECODEBANC", "ABC");
         System.out.println(s);
 
     }
@@ -26,9 +25,16 @@ public class SlidingWindow {
      *      说明：
      *          如果 S 中不存在这样的子串，则返回空串 ""
      *          如果 S 中存在这样的子串，我们保证它是唯一答案
+     *      滑动窗口算法的思路：
+     *          1、我们在字符串 S 中使用双指针中的左右指针技巧，初始化 left = right = 0，把索引左闭右开区间 [left, right) 称为一个「窗口」。
+     *          2、我们先不断地增加 right 指针扩大窗口 [left, right)，直到窗口中的字符串符合要求（包含了 T 中的所有字符）。
+     *          3、此时，我们停止增加 right，转而不断增加 left 指针缩小窗口 [left, right)，直到窗口中的字符串不再符合要求（不包含 T 中的所有字符了）。同时，每次增加 left，我们都要更新一轮结果。
+     *          4、重复第 2 和第 3 步，直到 right 到达字符串 S 的尽头。
+     *              这个思路其实也不难，第 2 步相当于在寻找一个「可行解」，然后第 3 步在优化这个「可行解」，最终找到最优解，也就是最短的覆盖子串。
+     *              左右指针轮流前进，窗口大小增增减减，窗口不断向右滑动，这就是「滑动窗口」这个名字的来历。
      * @return
      */
-    public String minWindowSubStr(String source, String target) {
+    public static String minWindowSubStr(String source, String target) {
 
         Map<Character, Integer> need = new HashMap<>();
         Map<Character, Integer> window = new HashMap<>();
@@ -67,10 +73,10 @@ public class SlidingWindow {
                 left ++;
                 //进行窗口内数据的一系列更新
                 if (need.containsKey(d)) {
+                    window.put(d, window.get(d) - 1);
                     if (window.get(d).equals(need.get(d))) {
                         valid --;
                     }
-                    window.put(d, window.get(d) - 1);
                 }
             }
 
